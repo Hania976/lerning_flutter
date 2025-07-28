b2b759f74548e61ea863212d7ed8f1e830bbfb10
Hania976 <haniaayoub8@gmail.com>
2025-07-28 22:46:45 +0200

    fix: Change background color of Scaffold to red

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(backgroundColor: Colors.blueAccent,
+  runApp(MaterialApp(home: Scaffold(backgroundColor: Colors.red,
   
   ),));
 }
