f93e3be36d25ccb3121ec7a71ddc2df5c8463cab
Hania976 <haniaayoub8@gmail.com>
2025-07-29 00:05:02 +0200

    refactor: Encapsulate print functions within Comminication class

--- lib/fonction.dart
+++ lib/fonction.dart
@@ -1,10 +1,14 @@
-void printHi(String name ,int t){
+
+
+class Comminication{
+  static void printHi(String name ,int t){
   for(int i=0;i<t ;i++){
     print("$i hello $name");
   }
 }
-void printHello({String ? name ,int ? t}){
+static void printHello({String ? name ,int ? t}){
   for(int i=0;i<t ! ;i++){
     print("$i hello $name");
   }
+}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:hania/add.dart';
 import 'package:hania/fonction.dart';
 
 void main(){
-  printHi("hania",3);
-  printHello(name:"hania",t:1);
+
+  Comminication .printHi("hania",3);
+  Comminication .printHello(name:"hania",t:1);
   moudha3afa(5);
 }
