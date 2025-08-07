c26b56e0e4b9a9592072c24b577447a4b4492d2a
Hania976 <haniaayoub8@gmail.com>
2025-08-07 17:34:42 +0200

    refactor: Simplify ScreenHome widget by removing unused variables and improving state management

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:hania/view/screen_home.dart';
+import 'package:hania/view/screen_home.dart'; // Make sure this path is correct and the file contains ScreenHome widget
 
 void main(){
 runApp(MaterialApp(
--- lib/view/screen_home.dart
+++ lib/view/screen_home.dart
@@ -1,81 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:hania/view/result.dart';
 
 class ScreenHome extends StatefulWidget {
-   ScreenHome({super.key});
+  const ScreenHome({super.key});
 
   @override
   State<ScreenHome> createState() => _ScreenHomeState();
 }
 
 class _ScreenHomeState extends State<ScreenHome> {
-int a = 2;
-String name ="hania   ";
-void add() {
-    a = a + 1;
+  int a = 0;
+  List<String> tasbih = [
+    'subhan allah',
+    'alhamdullilah',
+    'allahu akbar',
+    'la ilaha illallah',
+  ];
+  void add() {
     setState(() {
-      
-    });
-  }
-  void remove() {
-    a = a - 1;
-    setState(() {
-      
+      a++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton:FloatingActionButton(
-        
-        onPressed:(){
-        Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => ScreenResult(result:a, name: name,)),
-  );
-      },child: Icon(Icons.file_download_done_outlined),) ,
-      appBar:AppBar(
-      title: Text('counter'),
-
-    ),
-    body:
-    Center( child: Column(mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 235, 149, 177),
+        title: Center(
+          child: Text(
+            'tasbih',
+            style: TextStyle(fontSize: 35, color: Colors.black),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircleAvatar(child: IconButton(onPressed:  (){ add(); }, icon: Icon(Icons.add)),backgroundColor: Colors.green,),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(name.trim(),style:TextStyle(fontSize:40 )),
+            Text(tasbih[a], style: TextStyle(fontSize: 35)),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  add();
+                });
+              },
+              child: CircleAvatar(
+                radius: 60,
+                backgroundColor: const Color.fromARGB(255, 235, 149, 177),
+                child: Text(
+                  '$a',
+                  style: TextStyle(fontSize: 35, color: Colors.black),
+                ),
+              ),
             ),
-            CircleAvatar (child: IconButton(onPressed:  (){ remove();}, icon: Icon(Icons.remove)),backgroundColor: Colors.red,),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  a = 0;
+                });
+              },
+              child: Text(
+                'clear',
+                style: TextStyle(
+                  fontSize: 35,
+                  color: const Color.fromARGB(255, 112, 91, 107),
+                ),
+              ),
+            ),
           ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(decoration: InputDecoration(border: OutlineInputBorder()) ,onChanged: (value) {
-name =value;
-          },),
-
-        ),
-        InkWell(onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ScreenResult(result: a, name: name)),
-          );
-        },
-          child: Expanded(
-            child: Container(
-              
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child:Center(child: Text('Go to result',style:TextStyle(color:const Color.fromARGB(255, 183, 210, 232))))
-            ),
-          ),
-        )
-    ]),) ,);
+      ),
+    );
   }
 }
